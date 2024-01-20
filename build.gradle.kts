024c6ef8f90d39f29a1f0f97edc06ce965d08fa7
etff <wish2rich@gmail.com>
2024-01-20 14:18:58 +0900

    chore: spring actuator 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,11 +35,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+
 }
 
 tasks.withType<KotlinCompile> {
